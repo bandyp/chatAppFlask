437351e1c2b7584942a7f9f8b87984a97f1a8b96
Andrew Phillips <nd201175@gmail.com>
2019-06-07 07:59:37 +0000

    minor changes

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from flask import Flask, redirect, render_template, request, session, url_for
 
 
 app = Flask(__name__)
-app.secret_key = os.getenv("randomstring123")
+app.secret_key = os.getenv("SECRET", "randomstring123")
 messages = []
 
 
@@ -12,6 +12,7 @@ def add_message(username, message):
     """Add messages to the `messages` list"""
     now = datetime.now().strftime("%H:%M:%S")
     messages.append({"timestamp": now, "from": username, "message": message})
+
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -25,16 +26,18 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/chat/<username>", methods = ["GET", "POST"])
+@app.route("/chat/<username>", methods=["GET", "POST"])
 def user(username):
-    """Display and add chat messages"""
-    
+    """Add and display chat messages"""
     if request.method == "POST":
         username = session["username"]
         message = request.form["message"]
         add_message(username, message)
         return redirect(url_for("user", username=session["username"]))
-    
-    return render_template("chat.html", username = username, chat_messages = messages)
 
-app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", "5000")), debug=False)
+    return render_template("chat.html", username=username,
+                           chat_messages=messages)
+
+
+app.run(host=os.getenv("IP", "0.0.0.0"),
+        port=int(os.getenv("PORT", "5000")), debug=False)
